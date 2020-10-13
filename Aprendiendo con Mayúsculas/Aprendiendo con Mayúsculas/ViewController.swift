c95fb042999bf5e438d9b95bac21fd047288e098
Luis Angeles <a01273884@itesm.mx>
2020-10-12 19:51:44 -0500

    First Commit
    Project Created

---
+++ Aprendiendo con Mayúsculas/Aprendiendo con Mayúsculas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aprendiendo con Mayúsculas
+//
+//  Created by Luis Alberto Angeles Morales on 12/10/20.
+//  Copyright © 2020 Luis Alberto Angeles Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Aprendiendo con Mayúsculas/Aprendiendo con MayúsculasTests/Aprendiendo_con_Mayu_sculasTests.swift
@@ -0,0 +1,34 @@
+//
+//  Aprendiendo_con_Mayu_sculasTests.swift
+//  Aprendiendo con MayúsculasTests
+//
+//  Created by Luis Alberto Angeles Morales on 12/10/20.
+//  Copyright © 2020 Luis Alberto Angeles Morales. All rights reserved.
+//
+
+import XCTest
+@testable import Aprendiendo_con_Mayu_sculas
+
+class Aprendiendo_con_Mayu_sculasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
