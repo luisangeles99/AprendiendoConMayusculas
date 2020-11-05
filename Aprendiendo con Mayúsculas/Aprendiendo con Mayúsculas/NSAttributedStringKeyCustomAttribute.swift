63d0645c6f4580f81a1e399cc2bcfcf2c8dea687
user180585 <user180585@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2020-11-05 01:18:02 -0500

    pushing view controler tap exercise

---
+++ Aprendiendo con Mayúsculas/Aprendiendo con Mayúsculas/NSAttributedStringKeyCustomAttribute.swift
@@ -0,0 +1,13 @@
+//
+//  NSAttributedStringKeyCustomAttribute.swift
+//  Aprendiendo con Mayúsculas
+//
+//  Created by user180585 on 11/1/20.
+//  Copyright © 2020 Luis Alberto Angeles Morales. All rights reserved.
+//
+
+import Foundation
+
+extension NSAttributedString.Key {
+    static let myAttributeName = NSAttributedString.Key(rawValue: "MyCustomAttribute")
+}
